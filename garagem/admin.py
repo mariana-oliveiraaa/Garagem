2a4776602c371b7ec81dbaea81da402c0a80b8d0
mariana-oliveiraaa <marianaoliveira9142@gmail.com>
2023-03-31 15:06:48 -0300

    Criacao modelo acessorio

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Marca, Categoria
+from .models import Acessorio, Marca, Categoria
 
+admin.site.register(Acessorio)
 admin.site.register(Marca)
 admin.site.register(Categoria)
