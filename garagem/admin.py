ad5f59ec7bcb11bb9de42a9268ce04b07763e86c
mariana-oliveiraaa <marianaoliveira9142@gmail.com>
2023-03-31 15:13:28 -0300

    Criacao modelo veiculo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Acessorio, Cor, Marca, Categoria
+from .models import Acessorio, Cor, Marca, Categoria, Veiculo
 
 admin.site.register(Acessorio)
 admin.site.register(Cor)
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -30,3 +30,17 @@ class Cor(models.Model):
     
     class Meta:
         verbose_name_plural = "cores"
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT,  related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT,  related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos") 
+    ano = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+    modelo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Modelo: {self.modelo} (Cor: {self.cor} - Ano: {self.ano} - Marca: {self.marca})"
+    
+    class Meta:
+        verbose_name = "veículo"
